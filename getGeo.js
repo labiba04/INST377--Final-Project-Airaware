69de024850ea7447b6cba045e5968c58f77f4298
labiba04 <143653461+labiba04@users.noreply.github.com>
2025-05-17 21:55:08 -0400

    Delete getGeo.js

--- getGeo.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const { city } = req.query;
-  const key = process.env.WEATHER_API;
-  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  res.json(data);
-}
